514036ecb547ddb240c9035d6631053999eedd8c
Muhammad Usman <usman@uw.edu>
2014-01-16 22:37:46 -0800

    C1

--- 01Hw/hw1-queries.sql
+++ 01Hw/hw1-queries.sql
@@ -1,6 +1,9 @@
-CREATE TABLE ACTOR(
-	id integer PRIMARY KEY,
-	fname text,
-	lname text,
-	gender char(1)
-);
+----------------------------
+--- Q C1
+----------------------------
+SELECT A.fname, A.lname
+FROM ACTOR AS A, MOVIE AS M, CASTS AS C
+WHERE A.id=C.pid AND M.id=C.mid AND M.name='Officer 444';
+----------------------------
+--- Returns 13 rows
+----------------------------
