3d147798c4ed79dd5d6f79d7e131a99f5edbcdf9
Muhammad Usman <usman@uw.edu>
2014-01-17 00:16:53 -0800

    q3

--- 01Hw/hw1-queries.sql
+++ 01Hw/hw1-queries.sql
@@ -18,3 +18,27 @@ WHERE G.genre='Film-Noir'
 	AND (M.year%4)=0;
 --- Returns 113 rows
 
+----------------------------
+--- Q C3
+----------------------------
+SELECT DISTINCT A.fname, A.lname
+FROM ACTOR AS A, CASTS AS C1, MOVIE AS M1, CASTS AS C2, MOVIE AS M2
+WHERE A.id=C1.pid AND C1.mid=M1.id
+	AND A.id=C2.pid AND C2.mid=M2.id
+	AND M1.year < 1900 AND M2.year > 2000;
+--- Returns 48 rows
+--- I ran the following query to see that the these actors are actually famous people 
+--- (usually) that have been shown in documentaries (or other non-fiction genres) as 
+--- themselves hence a new movie id is created for them even though they're not 
+--- neccessarily acting in them
+
+-- QUERY to see that same actor acting in movies more than 100 years apart are actually documentaries/news/shorts of themselves
+SELECT A.id, M.id, M.name, M.year, G.genre, C.role
+FROM ACTOR AS A, CASTS AS C, MOVIE AS M, GENRE AS G
+WHERE A.id=C.pid AND C.mid=M.id
+	AND A.fname='Christian IX'
+	AND M.id = G.mid;
+
+----------------------------
+--- Q C4
+----------------------------
