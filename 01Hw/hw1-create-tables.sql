220f9055eeb973cc1b37a8f4c4725bbaf956a5ec
Muhammad Usman <usman@uw.edu>
2014-01-16 21:49:58 -0800

    hw1 baseline

---
+++ 01Hw/hw1-create-tables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE ACTOR(
+	id integer PRIMARY KEY,
+	fname text,
+	lname text,
+	gender char(1)
+);
+\copy ACTOR from C:\Users\Muhammad\Desktop\hw1\imdb\actor-ascii.txt with delimiter '|' csv quote E'\n'
+
+CREATE TABLE MOVIE(
+	id integer PRIMARY KEY,
+	name text,
+	year integer
+);
+\copy MOVIE from C:\Users\Muhammad\Desktop\hw1\imdb\movie-ascii.txt with delimiter '|' csv quote E'\n'
+
+CREATE TABLE DIRECTORS(
+	id integer PRIMARY KEY,
+	fname text,
+	lname text
+);
+\copy DIRECTORS from C:\Users\Muhammad\Desktop\hw1\imdb\directors-ascii.txt with delimiter '|' csv quote E'\n'
+
+CREATE TABLE GENRE(
+	mid integer,
+	genre text
+);
+\copy GENRE from C:\Users\Muhammad\Desktop\hw1\imdb\genre-ascii.txt with delimiter '|' csv quote E'\n'
+
+CREATE TABLE CASTS(
+	pid integer REFERENCES ACTOR(id),
+	mid integer REFERENCES MOVIE(id),
+	role text
+);
+\copy CASTS from C:\Users\Muhammad\Desktop\hw1\imdb\casts-ascii.txt with delimiter '|' csv quote E'\n'
+
+CREATE TABLE MOVIE_DIRECTORS(
+	did integer REFERENCES DIRECTORS(id),
+	mid integer REFERENCES MOVIE(id)
+);
+\copy MOVIE_DIRECTORS from C:\Users\Muhammad\Desktop\hw1\imdb\movie_directors-ascii.txt with delimiter '|' csv quote E'\n'
