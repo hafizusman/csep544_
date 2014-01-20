afaf924a6ac5593e97c1e61b996fa8baec0b9cce
Muhammad Usman <usman@uw.edu>
2014-01-20 01:01:10 -0800

    Question B

---
+++ 01Hw/hw1-create-indexes.sql
@@ -0,0 +1,5 @@
+﻿CREATE UNIQUE INDEX index_actor_id ON ACTOR (id);
+CREATE UNIQUE INDEX index_movie_id ON MOVIE (id);
+CREATE INDEX index_casts_pid ON CASTS (pid);
+CREATE INDEX index_casts_mid ON CASTS (mid);
+CREATE INDEX index_casts_role ON CASTS (role);
