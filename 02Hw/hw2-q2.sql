f388b65e4660445fbb34ec408a10ad18671cde0d
Muhammad Usman <usman@uw.edu>
2014-01-26 20:48:22 -0800

    hw 2 baseline

---
+++ 02Hw/hw2-q2.sql
@@ -0,0 +1,55 @@
+﻿CREATE TABLE InsuranceCo(
+	name	text PRIMARY KEY,
+	phone	varchar(25)
+);
+
+CREATE TABLE Vehicle(
+	licenceplate	text PRIMARY KEY,
+	year		integer,
+	maxliability	integer,
+	maxlossdamag	integer,
+	insurer		name REFERENCES InsuranceCo,
+	owner		char(11) REFERENCES Person
+);
+
+CREATE TABLE Person(
+	ssn		char(11) PRIMARY KEY,
+	name		text
+);
+
+CREATE TABLE Driver(
+	licencenumber	text PRIMARY KEY,
+	id		char(11) REFERENCES Person
+);
+
+CREATE TABLE Drives(
+	licenceplate	text REFERENCES Car,
+	licencenumber	text REFERENCES NonProfessionalDriver
+);
+
+CREATE TABLE Car(
+	plate 		text PRIMARY KEY REFERENCES Vehicle,
+	make		text
+);
+
+CREATE TABLE Truck(
+	plate 		text REFERENCES Vehicle,
+	capacity	integer,
+	licenceNo	text REFERENCES ProfessionalDriver
+);
+
+CREATE TABLE NonProfessionalDriver(
+-- NOTE: here the assumption is that a person gets a driver, his/her entry is added to the Driver
+--		table. Once the driver owns a car, an entry is created in the NonProfessionalDriver
+--		relation since now he/she is eligible to drive.
+--	This also gives the flexibility of adding new attributes to the NonProfessionalDriver
+--	table without causing update anomalies in Drivers.
+--	Finally, a Driver can be both in the NonProfessionalDriver and ProfessionalDriver tables
+	licencenum	text PRIMARY KEY REFERENCES Driver
+
+);
+
+CREATE TABLE ProfessionalDriver(
+	licencenum	text PRIMARY KEY REFERENCES Driver,
+	medicalhistory	text
+);
---
+++ 02Hw/hw2-q5.sql
@@ -0,0 +1,44 @@
+﻿SELECT * FROM SALES; -- 426 rows
+
+SELECT DISTINCT name FROM SALES; -- 36 rows
+SELECT DISTINCT discount FROM SALES; -- 3 rows
+SELECT DISTINCT month FROM SALES; -- 12 rows
+SELECT DISTINCT price FROM SALES; -- 9 rows
+
+----------------------------------------------------------------
+-- Q4 part ii
+--
+-- Queries used to determine functional dependencies
+-- I first created a list of all possible sets of 
+-- attributes and ran a query similar to the ones below
+-- for all of them.
+----------------------------------------------------------------
+SELECT MAX(A)
+FROM	(
+	SELECT name, count(DISTINCT month) AS A
+	FROM SALES
+	GROUP BY name
+	) B
+; -- name is not FD month
+
+SELECT MAX(A)
+FROM	(
+	SELECT name, count(DISTINCT price) AS A
+	FROM SALES
+	GROUP BY name
+	) B
+; -- name->price
+
+
+SELECT MAX(A)
+FROM	(
+	SELECT month, count(DISTINCT discount) AS A
+	FROM SALES
+	GROUP BY month
+	) B
+; -- month->discount
+
+----------------------------------------------------------------
+-- Q4 part iii
+--
+----------------------------------------------------------------
